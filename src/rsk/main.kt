45977db42a33f60fd74288fe755b3a45bd3d034e
Christopher Helgeson <ChristopherHelgeson@hotmail.com>
2017-12-15 09:46:38 -0500

    add inputStream and printWriter to main

--- src/rsk/main.kt
+++ src/rsk/main.kt
@@ -1,8 +1,18 @@
 package rsk
 
+import java.io.File
+import java.io.InputStream
+
 fun main(args: Array<String>) {
 
-    //return dijkstra(startingLocation, targetLocation, edges)
+    // read text file containing supplied input data
+    val inputStream: InputStream = File("Doll-Delivery_Input_Data.txt").inputStream()
+    val inputString = inputStream.bufferedReader().use { it.readText() }
+    println(inputString)
+
+    // write text file containing expected output data
+    val outString: String = (dijkstra(startingLocation, targetLocation, edges)).toString()
+    File("Doll-Delivery_Output_Data.txt").printWriter().use { out -> out.println(outString) }
 
     println(dijkstra(startingLocation, targetLocation, edges))
 }
